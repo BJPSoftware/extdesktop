497800ea4c7a76302e619ea0f581bb487390407a
autumnal_wind <autumnal_wind@yeah.net>
2016-01-16 23:59:33 +0800

    增加了日志配置

--- src/main/java/com/ympcsoft/extdesktop/service/sys/SysUserService.java
+++ src/main/java/com/ympcsoft/extdesktop/service/sys/SysUserService.java
@@ -2,17 +2,19 @@ package com.ympcsoft.extdesktop.service.sys;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ympcsoft.extdesktop.dao.sys.*;
-import com.ympcsoft.extdesktop.domain.sys.*;
+import com.ympcsoft.extdesktop.dao.sys.SysUserDao;
+import com.ympcsoft.extdesktop.domain.sys.SysUser;
 
 @Transactional
 @Service
 public class SysUserService {
     private SysUserDao sysUserDao;
-    
+    private static final Logger Log = LoggerFactory.getLogger(SysUserService.class);
     @Resource
     public void setSysUerDao(SysUserDao sysUserDao){
         this.sysUserDao = sysUserDao;
@@ -20,7 +22,9 @@ public class SysUserService {
     
     public void addUser(SysUser sysUser){
         
+        //Log.info("开始增加");
         sysUserDao.addUser(sysUser);
+        //Log.info("增加 完成");
     }
 
 }
